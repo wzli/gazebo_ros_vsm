508343ad88ffb9ce3b83a7350bde1aed2b4a51a4
WenZheng Li <me@wenzheng.li>
2020-07-16 21:00:43 +0900

    added yaml-cpp

--- gazebo_vsm/src/gazebo_vsm.cpp
+++ gazebo_vsm/src/gazebo_vsm.cpp
@@ -1,4 +1,6 @@
 #include <gazebo_vsm/gazebo_vsm.hpp>
+#include <vsm/zmq_transport.hpp>
+#include <yaml-cpp/yaml.h>
 
 #include <exception>
 #include <iostream>
@@ -23,6 +25,23 @@ void GazeboVsm::onWorldCreated(std::string world_name) {
     if (!_world) {
         throw std::runtime_error("VSM plugin: physics::get_world() fail to return world");
     }
+    vsm::MeshNode::Config mesh_config{
+            vsm::msecs(1),     // peer update interval
+            vsm::msecs(1000),  // entity expiry interval
+            8000,         // entity updates size
+            {},           // ego sphere
+            {
+                    "node1",                  // name
+                    "udp://127.0.0.1:11611",  // address
+                    {0, 0},                   // coordinates
+                    0,                        // power radius
+                    1,                        // connection_degree
+                    20,                       // lookup size
+                    0,                        // rank decay
+            },
+            std::make_shared<vsm::ZmqTransport>("udp://*:11611"),  // transport
+            std::make_shared<vsm::Logger>(),                       // logger
+    };
 }
 
 GZ_REGISTER_SYSTEM_PLUGIN(GazeboVsm)

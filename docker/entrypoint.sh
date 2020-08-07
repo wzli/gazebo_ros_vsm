b824cf087f182d2daa09bb5d4dc3852ff3359c02
WenZheng Li <me@wenzheng.li>
2020-08-07 21:52:16 +0900

    working robot sync

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,9 @@ export VSM_BOOTSTRAP_PEER=${VSM_BOOTSTRAP_PEER:-${VSM_ADDRESS%.*}.2:$VSM_PORT}
 
 NODE_ID=$((${VSM_ADDRESS##*.} - 2))
 export VSM_NAME=${VSM_NAME:-robot_$NODE_ID}
+export SPAWN_X=${SPAWN_X:-$NODE_ID}
+export SPAWN_Y=${SPAWN_Y:-"-$NODE_ID"}
+export SPAWN_YAW=${SPAWN_YAW:-$NODE_ID}
 
 (cd ${GZWEB_WS}; npm start) &
 source ${CATKIN_WS}/devel/setup.bash

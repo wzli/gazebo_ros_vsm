27cb938f106b4c9842d146acb68436db04a8d148
WenZheng Li <me@wenzheng.li>
2020-08-12 00:05:42 +0900

    touched up docker ENV params

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,11 +4,11 @@ export VSM_ADDRESS=${VSM_ADDRESS:-$(hostname -i)}
 export VSM_PORT=${VSM_PORT:-11511}
 export VSM_BOOTSTRAP_PEER=${VSM_BOOTSTRAP_PEER:-${VSM_ADDRESS%.*}.2:$VSM_PORT}
 
-NODE_ID=$((${VSM_ADDRESS##*.} - 2))
-export VSM_NAME=${VSM_NAME:-robot_$NODE_ID}
-export SPAWN_X=${SPAWN_X:-$NODE_ID}
-export SPAWN_Y=${SPAWN_Y:-"-$NODE_ID"}
-export SPAWN_YAW=${SPAWN_YAW:-$NODE_ID}
+VSM_ID=${VSM_ID:-$((${VSM_ADDRESS##*.} - 2))}
+export VSM_NAME=${VSM_NAME:-robot_$VSM_ID}
+export SPAWN_X=${SPAWN_X:-$VSM_ID}
+export SPAWN_Y=${SPAWN_Y:-"-$VSM_ID"}
+export SPAWN_YAW=${SPAWN_YAW:-1.57}
 
 (cd ${GZWEB_WS}; npm start) &
 source ${CATKIN_WS}/devel/setup.bash
